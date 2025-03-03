62caa6bebdd1687b7d7ee787352c8392440e6ac0
mcurtiss1992 <69280569+mcurtiss1992@users.noreply.github.com>
2025-03-03 07:43:08 -0600

    More PWA

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "omote-config-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/omote_home.html",
+  "/omote_device_manager.html",
+  "/omote_device_config.html",
+  "/omote_global_config.html",
+  "omote_scene_manager.html",
+  "remote_config.html",
+  "/icons/icon-48x48.html",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Install service worker and cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event: Serve cached assets when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate service worker and remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
